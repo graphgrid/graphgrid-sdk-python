d394d4a3d7c1945ffed225c8884068f5ea8e2e44
Alex Iudice <alex.iudice@atomrain.com>
2022-02-10 06:21:26 -0500

    ability to get instances of modules based on string input like boto3 client.

---
+++ ggsdk/modules/factory.py
@@ -0,0 +1,76 @@
+import enum
+import typing
+from dataclasses import dataclass
+
+from ggsdk.modules.module import ConfigModule, SecurityModule, NlpModule
+
+CONFIG = 'config'
+SECURITY = 'security'
+NLP = 'nlp'
+
+
+@dataclass
+class GraphGridModuleFactory:
+    def create_module_instance(self):
+        pass
+
+
+class ConfigModuleFactory(GraphGridModuleFactory):
+    def __init__(self, ):
+        print("config fact")
+
+    def create_module_instance(self):
+        return ConfigModule()
+
+
+class SecurityModuleFactory(GraphGridModuleFactory):
+    def __init__(self, ):
+        print("sec fact")
+
+    def create_module_instance(self):
+        return SecurityModule()
+
+
+class NlpModuleFactory(GraphGridModuleFactory):
+    def __init__(self, ):
+        print("nlp factory")
+
+    def create_module_instance(self):
+        return NlpModule()
+
+
+class ModuleFactoryFactory(enum.Enum):
+    config = CONFIG
+    security = SECURITY
+    nlp = NLP
+
+    _factory: GraphGridModuleFactory
+
+    def __init__(self, value):
+        super().__init__()
+
+        options = {
+            CONFIG: self._config(),
+            SECURITY: self._security(),
+            NLP: self._nlp(),
+        }
+        if self.value in options:
+            self._factory = options[self.value]
+        else:
+            raise Exception("bad module str")
+
+    def _config(self):
+        return ConfigModuleFactory()
+
+    def _security(self):
+        return SecurityModuleFactory()
+
+    def _nlp(self):
+        return NlpModuleFactory()
+
+    def create_module(self):
+        return self._factory.create_module_instance()
+
+
+def module(s: str):
+    return ModuleFactoryFactory(s).create_module()
--- ggsdk/modules/module.py
+++ ggsdk/modules/module.py
@@ -1,16 +1,4 @@
-import enum
-import typing
 from dataclasses import dataclass
-
-CONFIG = 'config'
-SECURITY = 'security'
-NLP = 'nlp'
-
-
-@dataclass
-class GraphGridModuleFactory:
-    def create_module_instance(self):
-        pass
 
 
 @dataclass
@@ -32,60 +20,3 @@ class NlpModule(GraphGridModule):
 class SecurityModule(GraphGridModule):
     def __init__(self, ):
         print("SECURITY SDK MODULE CREATED")
-
-
-class ConfigModuleFactory(GraphGridModuleFactory):
-    def __init__(self, ):
-        print("config fact")
-
-    def create_module_instance(self):
-        return ConfigModule()
-
-
-class SecurityModuleFactory(GraphGridModuleFactory):
-    def __init__(self, ):
-        print("sec fact")
-
-    def create_module_instance(self):
-        return SecurityModule()
-
-
-class NlpModuleFactory(GraphGridModuleFactory):
-    def __init__(self, ):
-        print("nlp factory")
-
-    def create_module_instance(self):
-        return NlpModule()
-
-
-class ModuleFactoryFactory(enum.Enum):
-    config = CONFIG
-    security = SECURITY
-    nlp = NLP
-
-    _factory: GraphGridModuleFactory
-
-    def __init__(self, value):
-        super().__init__()
-
-        options = {
-            CONFIG: self._config(),
-            SECURITY: self._security(),
-            NLP: self._nlp(),
-        }
-        if self.value in options:
-            self._factory = options[self.value]
-        else:
-            raise Exception("bad module str")
-
-    def _config(self):
-        return ConfigModuleFactory()
-
-    def _security(self):
-        return SecurityModuleFactory()
-
-    def _nlp(self):
-        return NlpModuleFactory()
-
-    def create_module(self):
-        return self._factory.create_module_instance()
