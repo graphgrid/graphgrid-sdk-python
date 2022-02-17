5805fcaa48200cd8bb9ec1c07fdf3d0b31aa7307
Alex Iudice <alex.iudice@atomrain.com>
2022-02-17 12:38:52 -0500

    Adding sdk class for main driver.

---
+++ ggsdk/sdk.py
@@ -0,0 +1,29 @@
+import ggcore.client_factory
+from ggcore.client import NlpClient
+
+
+class SdkMain:
+
+    _nlp_client: NlpClient
+    def __init__(self):
+        # get bootstrap config
+
+        # Get config client
+
+        # Get security client
+            # Get url base, setup
+
+
+        _nlp_client = ggcore.client_factory.client("nlp") # this seems wrong
+
+    def save(self):
+        pass
+
+    def train(self):
+        pass
+
+    def get_job_status(self):
+        pass
+
+    def get_job_results(self):
+        pass
