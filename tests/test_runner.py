ef3bd751ed40f24de5cc3da5c2a6305c28e9e24e
harrydunham <harry.dunham@atomrain.com>
2022-02-22 10:22:26 -0500

    setup docker testing paradigm

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,7 +1,7 @@
 """Define methods to run tests and configure test output"""
 import os
+import subprocess
 from pathlib import Path
-import pytest
 from clover.coverage2clover import Clover, Cobertura
 
 
@@ -12,13 +12,13 @@ def main():
     pytest_path = os.path.join(test_dir, "pytest.xml")
     cov_path = os.path.join(test_dir, "cov.xml")
     clover_path = os.path.join(test_dir, "clover.xml")
-    coveragerc_path = os.path.join(test_dir, ".coveragerc")
-    pytest.main(["--junitxml=" + pytest_path,
-                 "--cov-report", "xml:" + cov_path,
-                 "--cov=" + str(top_level_dir),
-                 "--cov-config=" + coveragerc_path,
-                 test_dir,
-                 ])
+    coveragerc_path = os.path.join(top_level_dir, ".coveragerc")
+    subprocess.check_call("pytest"
+                          " --junitxml=" + pytest_path +
+                          " --cov-report xml:" + cov_path +
+                          " --cov=" + str(top_level_dir) +
+                          " --cov-config=" + coveragerc_path +
+                          " " + test_dir, shell=True)
     cov = Cobertura()
     cov.open(cov_path)
     cl = Clover(cov)
