dc2cb03f86c82b759e0bbddf61753aeb42e2b0b7
Alex Iudice <alex.iudice@atomrain.com>
2022-03-15 14:58:27 -0400

    Linting client_factory.py

--- ggcore/client_factory.py
+++ ggcore/client_factory.py
@@ -1,3 +1,5 @@
+"""Classes for sdk client factory (unused)"""
+
 import typing
 from dataclasses import dataclass
 
@@ -6,35 +8,47 @@ from ggcore.client import ConfigClient, SecurityClient, NlpClient, ClientBase
 from ggcore.utils import CONFIG, SECURITY, NLP
 
 
-# todo remove this file? tempted to keep it around for the future
+# todo remove this file? could be used in the future, but currently unused
 
+# pylint: disable=too-few-public-methods
 class GraphGridModuleClientFactory:
+    """Client factory base class"""
+
     def create_client_instance(self) -> ClientBase:
-        pass
+        """Create a client instance"""
 
 
 class ConfigClientFactory(GraphGridModuleClientFactory):
+    """Config client factory"""
+
     def create_client_instance(self) -> ClientBase:
-        return ConfigClient()
+        return ConfigClient(None)
 
 
 class SecurityClientFactory(GraphGridModuleClientFactory):
+    """Security client factory"""
+
     def create_client_instance(self) -> ClientBase:
-        return SecurityClient()
+        return SecurityClient(None)
 
 
 class NlpClientFactory(GraphGridModuleClientFactory):
+    """Nlp client factory"""
+
     def create_client_instance(self) -> ClientBase:
-        return NlpClient()
+        return NlpClient(None)
 
 
 @dataclass
 class FactoryData:
+    """Factory information"""
     client_name: str
     factory: GraphGridModuleClientFactory
 
 
 class ClientFactory:
+    """Creates clients from client factories"""
+
     _FACTORIES_DATA: typing.Dict[str, FactoryData] = {
         CONFIG: FactoryData(CONFIG, ConfigClientFactory()),
         SECURITY: FactoryData(SECURITY, SecurityClientFactory()),
@@ -43,18 +57,18 @@ class ClientFactory:
 
     @classmethod
     def call_module_client_factory(cls, validated_client_name) -> ClientBase:
+        """Call to get the client factory and create a client instance"""
         return cls._FACTORIES_DATA.get(
             validated_client_name).factory.create_client_instance()
 
     @classmethod
     def create_client(cls, client_name) -> ClientBase:
-        """create client from string"""
+        """Create client from string"""
         if client_name in cls._FACTORIES_DATA:
             return cls.call_module_client_factory(client_name)
-        else:
-            raise sdk_exceptions.SdkInvalidClient
+        raise sdk_exceptions.SdkInvalidClient
 
 
-def client(s: str) -> ClientBase:
-    """create client from string"""
-    return ClientFactory.create_client(s)
+def client(client_name: str) -> ClientBase:
+    """Create client from string (unused)"""
+    return ClientFactory.create_client(client_name)
