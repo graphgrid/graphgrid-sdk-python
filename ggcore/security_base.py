8dd80302c6d1fd9adb3fef583c8416bcabd44f3f
Alex Iudice <alex.iudice@atomrain.com>
2022-03-15 14:14:24 -0400

    Lints security_base.py

--- ggcore/security_base.py
+++ ggcore/security_base.py
@@ -1,3 +1,5 @@
+"""Classes for request auth and building auth headers"""
+
 import base64
 from dataclasses import dataclass
 
@@ -7,23 +9,29 @@ from ggcore.utils import AUTH_HEADER_KEY, BASIC_HEADER_KEY, BEARER_HEADER_KEY
 
 @dataclass
 class RequestAuth:
+    """Base class for the different request auth types"""
     security_config: SdkSecurityConfig
 
     def get_auth_value(self) -> str:
-        pass
+        """Returns the authentication value"""
 
     def get_auth_key(self) -> str:
-        pass
+        """Returns the authentication key"""
 
     def get_auth_header(self) -> dict:
+        """Builds and returns the authentication header"""
         return {
             AUTH_HEADER_KEY: f'{self.get_auth_key()} {self.get_auth_value()}'}
 
 
 @dataclass
 class BasicAuth(RequestAuth):
+    """Basic auth class"""
+
     def get_auth_value(self) -> str:
-        key_secret_string = f'{self.security_config.access_key}:{self.security_config.secret_key}'
+        key_secret_string = f'{self.security_config.access_key}:' \
+                            f'{self.security_config.secret_key}'
+
         b64_encoded_basic_auth = base64.b64encode(
             f'{key_secret_string}'.encode())
         return b64_encoded_basic_auth.decode()
@@ -34,6 +42,8 @@ class BasicAuth(RequestAuth):
 
 @dataclass
 class BearerAuth(RequestAuth):
+    """Bearer auth class"""
+
     def get_auth_value(self) -> str:
         return self.security_config.token
 
@@ -42,10 +52,14 @@ class BearerAuth(RequestAuth):
 
 
 class SdkAuthHeaderBuilder:
+    """Builds auth headers"""
+
     @classmethod
-    def get_basic_header(self, sec_conf: SdkSecurityConfig) -> dict:
+    def get_basic_header(cls, sec_conf: SdkSecurityConfig) -> dict:
+        """Returns the basic auth for the provided security config"""
         return BasicAuth(sec_conf).get_auth_header()
 
     @classmethod
-    def get_bearer_header(self, sec_conf: SdkSecurityConfig) -> dict:
+    def get_bearer_header(cls, sec_conf: SdkSecurityConfig) -> dict:
+        """Returns the bearer auth for the provided security config"""
         return BearerAuth(sec_conf).get_auth_header()
