17855fe00ba44beb9cdab3cd5b735b4a72cbe37d
Alex Iudice <alex.iudice@atomrain.com>
2022-03-18 12:16:10 -0400

    Relints ggcore/core.py based on comments.

--- ggcore/core.py
+++ ggcore/core.py
@@ -1,4 +1,4 @@
-"""The SDK core entrypoint for sdk calls"""
+"""Define the SDK core entrypoint for sdk calls."""
 import typing
 
 from ggcore.client import ConfigClient, NlpClient
@@ -6,8 +6,9 @@ from ggcore.config import SdkBootstrapConfig
 
 
 class SdkCore:
-    """Holds sdk configuration plus all clients used for the sdk. Executes
-    the calls coming from GraphGridSdk. """
+    """Define core sdk class to hold sdk configuration plus all clients used
+    for the sdk. Execute the calls coming from GraphGridSdk.
+    """
     _configuration: SdkBootstrapConfig
 
     _config_client: ConfigClient
@@ -19,18 +20,18 @@ class SdkCore:
         self._setup_clients()
 
     def _setup_clients(self, ):
-        """Setup low-level clients"""
+        """Setup low-level clients."""
         self._config_client = ConfigClient(self._configuration)
         self._nlp_client = NlpClient(self._configuration)
 
     # test purposes only
     def test_api(self):
-        """Execute test call"""
+        """Execute test call."""
         return self._config_client.test_api()
 
     def save_dataset(self, generator: typing.Generator, dataset_id: str,
                      overwrite: bool):
-        """Execute save dataset call"""
+        """Execute save dataset call."""
         return self._nlp_client.save_dataset(generator=generator,
                                              dataset_id=dataset_id,
                                              overwrite=overwrite)
