b4e036433672a6e3049b0f6be009149d0568f0c2
Alex Iudice <alex.iudice@atomrain.com>
2022-03-18 12:27:10 -0400

    Relints ggcore/session.py based on comments.

--- ggcore/session.py
+++ ggcore/session.py
@@ -1,15 +1,15 @@
-"""Classes around session tracking and token management"""
+"""Define classes around session tracking and token management."""
 import typing
 
 
 # pylint: disable=too-few-public-methods
 class TokenFactory:
-    """TokenFactory is the dynamic caller for getting a token"""
+    """Define class to dynamically call for a token."""
     _token_supplier: typing.Callable[[], str]
 
     def __init__(self, token_supp):
         self._token_supplier = token_supp
 
     def get_token_from_request(self) -> str:
-        """get token from the token supplier"""
+        """Get token from the token supplier."""
         return self._token_supplier()
