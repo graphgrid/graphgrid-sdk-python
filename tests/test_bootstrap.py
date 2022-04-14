007a4b9a493a1ddb57d91be742520bbece79b9d6
Alex Iudice <alex.iudice@atomrain.com>
2022-04-14 13:02:50 -0400

    Test cleanup/refactor.

--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -1,21 +1,22 @@
+"""Define test classes for SDK bootstrapping."""
 import os
 from unittest import mock
 
 from graphgrid_sdk.ggsdk.sdk import GraphGridSdk
-from tests.test_base import TestBootstrapBase
+from tests.test_base import TestBootstrapBase, TestBase
 
 
-class TestBootstrapConfig(TestBootstrapBase):
-    __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+class TestBootstrapAutoConfig(TestBootstrapBase):
+    """Define class for testing the boostrap autoconfig."""
 
+    # pylint: disable=broad-except,no-self-use
     @mock.patch.dict(os.environ, {
-        "GRAPHGRID_CONFIG_CREDENTIALS_PATH": f"{__location__}/resources",
-        "CONFIG_CREDENTIAL_PROPERTIES_FILENAME": "configCredentials.properties"}
-        , clear=True)
+        "GRAPHGRID_CONFIG_CREDENTIALS_PATH": f"{TestBase.TEST_DIR_LOCATION}/resources",
+        "CONFIG_CREDENTIAL_PROPERTIES_FILENAME": "configCredentials.properties"})
     def test_auto_config(self):
+        """Define test that automagically sets up GraphGridSdk config."""
         try:
             GraphGridSdk()
         except Exception:
             assert False, \
-                f"Hit error while setting up SDK using autoconfig bootstrapping."
+                "Hit error while setting up SDK using autoconfig bootstrapping."
